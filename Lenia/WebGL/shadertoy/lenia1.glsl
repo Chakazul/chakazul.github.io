d4214973a717353a5d5c13c5274cd2810ea96672
Bert Chan <36795411+Chakazul@users.noreply.github.com>
2025-10-01 19:07:02 +0900

    change initial density

--- Lenia/WebGL/shadertoy/lenia1.glsl
+++ Lenia/WebGL/shadertoy/lenia1.glsl
@@ -2,8 +2,8 @@
 
 vec2 hash( vec2 p )
 {
-	p = vec2( dot(p,vec2(127.1,311.7)), dot(p,vec2(269.5,183.3)) );
-	return -1.0 + 2.0*fract(sin(p)*43758.5453123);
+    p = vec2( dot(p,vec2(127.1,311.7)), dot(p,vec2(269.5,183.3)) );
+    return -1.0 + 2.0*fract(sin(p)*43758.5453123);
 }
 
 float noise( in vec2 p )
@@ -11,14 +11,14 @@ float noise( in vec2 p )
     const float K1 = 0.366025404; // (sqrt(3)-1)/2;
     const float K2 = 0.211324865; // (3-sqrt(3))/6;
 
-	vec2  i = floor( p + (p.x+p.y)*K1 );
+    vec2  i = floor( p + (p.x+p.y)*K1 );
     vec2  a = p - i + (i.x+i.y)*K2;
     float m = step(a.y,a.x); 
     vec2  o = vec2(m,1.0-m);
     vec2  b = a - o + K2;
-	vec2  c = a - 1.0 + 2.0*K2;
+    vec2  c = a - 1.0 + 2.0*K2;
     vec3  h = max( 0.5-vec3(dot(a,a), dot(b,b), dot(c,c) ), 0.0 );
-	vec3  n = h*h*h*h*vec3( dot(a,hash(i+0.0)), dot(b,hash(i+o)), dot(c,hash(i+1.0)));
+    vec3  n = h*h*h*h*vec3( dot(a,hash(i+0.0)), dot(b,hash(i+o)), dot(c,hash(i+1.0)));
     return dot( n, vec3(70.0) );
 }
 
@@ -61,11 +61,11 @@ void mainImage( out vec4 fragColor, in vec2 fragCoord )
     float c = clamp(val + dt * growth, 0., 1.);
 
     if (iFrame < 1) // || iMouse.z > 0.
-        c = 0.013 + noise(fragCoord/R + mod(iDate.w,1.)*100.);
+        c = 0.7 * noise(fragCoord/R + mod(iDate.w,1.)*10.);
     if (iMouse.z > 0.) {
         float d = length((fragCoord.xy - iMouse.xy) / iResolution.xx);
-        if (d <= R/iResolution.x)
-        	c = 0.02 + noise(fragCoord/R + mod(iDate.w,1.)*100.);
+        if (d <= R/iResolution.x*1.5)
+            c = 1.2 * noise(fragCoord/R + mod(iDate.w,1.)*10.);
     }
 
     fragColor = vec4(c,c,c,1.);
